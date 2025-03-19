b1ecd0fdfaa73468954ed851ea31e2f510398915
pablloedu <pablloedu@gmail.com>
2025-03-19 17:04:52 -0300

    criacao das models do estoque

--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    categoria = models.CharField(max_length=100)
+    nome = models.CharField(max_length=200)
+    quantidade_estoque = models.PositiveIntegerField()
+    quantidade_pacote = models.PositiveIntegerField()
+    preco_compra = models.DecimalField(max_digits=10, decimal_places=3)
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=3)
+    margem_lucro = models.FloatField()
+
+    def __str__(self):
+        return self.nome
