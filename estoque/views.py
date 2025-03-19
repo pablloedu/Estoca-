8dd95cdc3d2d42c2b0d7baf99e2dda31e514ebd4
pablloedu <pablloedu@gmail.com>
2025-03-19 17:34:25 -0300

    criação das views

--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Produto
 # Create your views here.
+
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render (request, 'estoque/listar_produtos.html', {'produtos': produtos}) 
