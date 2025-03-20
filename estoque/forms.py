bcc8e2108c3f93f4c9905b7fea71d88fd4c0f916
pablloedu <pablloedu@gmail.com>
2025-03-20 15:29:04 -0300

    calculo do preço dinamico

--- estoque/forms.py
+++ estoque/forms.py
@@ -4,4 +4,4 @@ from .models import Produto
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
-        fields = ['categoria', 'nome', 'quantidade_estoque', 'quantidade_pacote', 'preco_compra', 'preco_venda', 'margem_lucro']
+        fields = ['categoria', 'nome', 'quantidade_estoque', 'quantidade_pacote', 'preco_compra', 'margem_lucro']
--- estoque/models.py
+++ estoque/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 class Produto(models.Model):
     categoria = models.CharField(max_length=100)
-    nome = models.CharField(max_length=200)
-    quantidade_estoque = models.PositiveIntegerField()
-    quantidade_pacote = models.PositiveIntegerField()
-    preco_compra = models.DecimalField(max_digits=10, decimal_places=3)
-    preco_venda = models.DecimalField(max_digits=10, decimal_places=3)
-    margem_lucro = models.FloatField()
+    nome = models.CharField(max_length=255)
+    quantidade_estoque = models.IntegerField()
+    quantidade_pacote = models.IntegerField()
+    preco_compra = models.DecimalField(max_digits=10, decimal_places=2)
+    margem_lucro = models.DecimalField(max_digits=5, decimal_places=2)
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def calcular_preco_venda(self):
+        if self.preco_compra and self.margem_lucro:
+            self.preco_venda = self.preco_compra * (1 + (self.margem_lucro / 100))
+
+    def save(self, *args, **kwargs):
+        self.calcular_preco_venda()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nome
