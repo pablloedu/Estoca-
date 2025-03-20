0301ff0469bb306a7681c4c7a32ff772f6287dcb
pablloedu <pablloedu@gmail.com>
2025-03-20 14:45:46 -0300

    cadastro de produtos

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_produtos
+from .views import listar_produtos, cadastrar_produto
 
 urlpatterns = [
-    path('', listar_produtos, name='listar_produtos')
+    path('', listar_produtos, name='listar_produtos'),
+    path('cadastrar/', cadastrar_produto, name='cadastrar_produto'),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 from .models import Produto
+from .forms import ProdutoForm
 # Create your views here.
 
 def listar_produtos(request):
     produtos = Produto.objects.all()
     return render (request, 'estoque/listar_produtos.html', {'produtos': produtos}) 
+
+def cadastrar_produto(request):
+    if request.method == "POST":
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'estoque/cadastrar_produto.html', {'form': form})
+
